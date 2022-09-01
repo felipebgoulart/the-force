e06fc0b837cf6af9e66f75c77906917d07e7c471
Felipe Goulart <felipe.goulart1999@gmail.com>
2022-09-01 15:45:24 -0300

    FEAT: coverage ci

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/folio'),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+      reporters: [{ type: 'json' }, { type: 'html' }, { type: 'text-summary' }],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
